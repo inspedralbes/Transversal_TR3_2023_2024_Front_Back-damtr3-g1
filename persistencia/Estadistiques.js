41dece3bd0ffa605e09cfb9821895abd1ed2a78d
a22biepalgon <a22biepalgon@inspedralbes.cat>
2024-04-02 10:13:35 +0200

    canvis persistencia

---
+++ persistencia/Estadistiques.js
@@ -0,0 +1,35 @@
+bdConnexio = require("./Connexio.js");
+conn = bdConnexio.conn;
+
+module.exports = {
+    getEstadistiques: function(id){
+        var sql = 'SELECT * FROM Estadisticas WHERE idUser = ' + id;
+        return new Promise((resolve, reject) =>{
+            conn.query(sql, (err, result) => {
+                if (err) {
+                    reject(err)
+                }else{
+                    resolve(result)
+                }
+            });
+        })
+    },
+
+    updateEstadistiques: function(partidasJugadas, tiempoPartida, tiempoJugado, totalkills, killsPartida, totalDeaths, deathsPartida, totalAssists, assistsPartida, totalWins, winsPartida){
+        var sql = 'UPDATE Estadisticas SET PartidasJugadas = ' + (partidasJugadas + 1) + ', TiempoPartida = ' + (tiempoPartida + tiempoJugado + ', kills = ' + ( totalkills + killsPartida) + ', deaths = ' + (totalDeaths + deathsPartida) + ', assists = ' + (totalAssists + assistsPartida) + ', wins = ' + (totalWins + winsPartida))
+        return new Promise((resolve, reject) =>{
+            conn.query(sql, (err, result) => {
+                if (err) {
+                    reject(err)
+                }else{
+                    resolve(result)
+                }
+            });
+        })
+    }
+   
+}
+
+
+
+
