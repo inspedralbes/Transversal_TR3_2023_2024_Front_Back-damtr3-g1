d00345c28e1a3d1656502034bea13c0e02046837
a22fravilcas <a22fravilcas@inspedralbes.cat>
2024-04-06 13:26:11 +0200

    dist generat

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -4,9 +4,6 @@ import { decode, parsePath, withoutBase, withoutTrailingSlash, normalizeURL } fr
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from '..\\layouts\\error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
-
-import '..\\node_modules\\vuetify\\dist\\vuetify.css'
 
 import _777c3502 from '..\\layouts\\HomeLayout.vue'
 import _1ee80a1d from '..\\layouts\\LoginLayout.vue'
@@ -48,7 +45,7 @@ export default {
       }
     }, [
       loadingEl,
-      h(NuxtBuildIndicator),
+
       transitionEl
     ])
   },
@@ -99,6 +96,10 @@ export default {
 
     isFetching () {
       return this.nbFetching > 0
+    },
+
+    isPreview () {
+      return Boolean(this.$options.previewData)
     },
   },
 
@@ -189,10 +190,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
