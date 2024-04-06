e26b7bde5eb45e8f3c8bfc2f2ac7e298fc007670
a22fravilcas <a22fravilcas@inspedralbes.cat>
2024-04-06 13:08:30 +0200

    node_modules

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,10 +4,10 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _b0baef3a = () => interopDefault(import('..\\pages\\dashboard.vue' /* webpackChunkName: "pages/dashboard" */))
-const _59832460 = () => interopDefault(import('..\\pages\\home.vue' /* webpackChunkName: "pages/home" */))
-const _462c15e9 = () => interopDefault(import('..\\pages\\procesos.vue' /* webpackChunkName: "pages/procesos" */))
-const _2b1eab21 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _452f0959 = () => interopDefault(import('..\\pages\\dashboard.vue' /* webpackChunkName: "pages/dashboard" */))
+const _28db61aa = () => interopDefault(import('..\\pages\\home.vue' /* webpackChunkName: "pages/home" */))
+const _09309e33 = () => interopDefault(import('..\\pages\\procesos.vue' /* webpackChunkName: "pages/procesos" */))
+const _46ce1717 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -22,19 +22,19 @@ export const routerOptions = {
 
   routes: [{
     path: "/dashboard",
-    component: _b0baef3a,
+    component: _452f0959,
     name: "dashboard"
   }, {
     path: "/home",
-    component: _59832460,
+    component: _28db61aa,
     name: "home"
   }, {
     path: "/procesos",
-    component: _462c15e9,
+    component: _09309e33,
     name: "procesos"
   }, {
     path: "/",
-    component: _2b1eab21,
+    component: _46ce1717,
     name: "index"
   }],
 
